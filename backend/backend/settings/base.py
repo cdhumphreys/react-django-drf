86ba1e678a02f3966b5e7c63f974ccccc4851eef
carlhumphreys <carl.humphreys@quotevineapp.com>
2020-02-05 13:47:50 +0000

    adding DRF, users serializer & view, freezing pip packages

--- backend/backend/settings/base.py
+++ backend/backend/settings/base.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'base'
+    'base',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,14 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 ROOT_URLCONF = 'backend.urls'
 
