4ce47ed949b61218e44ddd4fdd9b5c19c0acf37a
carlhumphreys <carl.humphreys@quotevineapp.com>
2020-02-05 13:33:06 +0000

    django backend setup
    some settings & structure changed

---
+++ backend/backend/settings/__init__.py
@@ -0,0 +1,4 @@
+from .base import *
+
+# Should change based on environment
+from .dev import *
---
+++ backend/backend/settings/staging.py
@@ -0,0 +1,7 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Staging site IP address & domains etc
+ALLOWED_HOSTS = []
